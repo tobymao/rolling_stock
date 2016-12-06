2389c70abee8a7c662c5123c8723ee5f6b90f771
Gary Franczyk <gkf42@yahoo.com>
2016-12-06 00:40:22 -0500

    updated corporation and rspec

--- spec/models/corporation_spec.rb
+++ spec/models/corporation_spec.rb
@@ -24,5 +24,59 @@ describe Corporation do
       player.companies << company_to_buy
       expect { subject.buy_company company_to_buy, 10}.to change { player.companies.size }.by(-1)
     end
+
+    it 'can_buy_shares should return false if bank shares is empty' do
+      expect(subject.can_buy_share?).to eq(true)
+    end
+
   end
+
+  describe '#is_bankrupt' do
+    it 'company is not bankrupt' do
+      expect(subject.is_bankrupt?).to eq(false)
+    end
+  end
+
+  describe "#issue_share" do
+    it "can issue a share" do
+      expect(subject.can_issue_share?).to be(true)
+    end
+
+    it "adds money to cash" do
+      expect { subject.issue_share }.to change { subject.cash }.by(9)
+    end
+
+    it "reduces the number of available shares" do
+      expect { subject.issue_share }.to change { subject.shares.size }.by(-1)
+    end
+  end
+
+  describe '#close_company' do
+    it 'removes the company from the corporation.' do
+      expect { subject.close_company company }.to change { subject.companies.size }.by(-1)
+    end
+  end
+
+  describe '#collect income' do
+    it 'increases cash' do
+      expect(subject.companies.size).to eq(1)
+      expect { subject.collect_income :red }.to change { subject.cash }.by(1)
+    end
+  end
+
+  # Don't know how to create and pass in an array
+  #describe '#pay_dividend' do
+
+  describe '#book_value' do
+    it 'is worth some money' do
+      expect(subject.book_value).to eq(20)
+    end
+  end
+
+  describe '#market_cap' do
+    it 'returns a value' do
+      expect(subject.market_cap).to eq(20)
+    end
+  end
+
 end
