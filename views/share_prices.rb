88e5b83d5d5e607efb6306e547ad200cf2f5ce14
Toby Mao <toby.mao@gmail.com>
2016-12-19 21:10:27 -0800

    hacky stock market

--- views/share_prices.rb
+++ views/share_prices.rb
@@ -13,7 +13,19 @@ module Views
     end
 
     def render_share_price share_price
-      div share_price.price
+      share_style = inline(
+        display: 'inline-block',
+        margin: '5px',
+        vertical_align: 'top',
+        border: 'solid 1px rgba(0,0,0,0.2)',
+        width: '60px',
+        height: '40px',
+      )
+
+      div style: share_style do
+        div share_price.price
+        div share_price.corporation&.name
+      end
     end
 
   end
