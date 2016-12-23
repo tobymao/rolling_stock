e48beedbe1af351303a22f099383e76a4444bfed
Gary Franczyk <gkf42@yahoo.com>
2016-12-23 01:10:23 -0500

    Cleaned up index list of games

--- views/index.rb
+++ views/index.rb
@@ -14,7 +14,10 @@ module Views
     end
 
     def render_new_game
-      div 'New Game'
+      hr
+      div style: inline(table_style.merge('font-weight': 'bold', 'width': '200px')) do
+        text 'New Game'
+      end
 
       form action: '/game', method: 'post' do
         rawtext app.csrf_tag
@@ -23,28 +26,86 @@ module Views
     end
 
     def render_join_games
-      div 'Join Game'
+      hr
+      div style: inline(table_style.merge('font-weight': 'bold', 'width': '200px')) do
+        text 'Current Games'
+      end
+
+      div do
+        span style: inline(table_style.merge('font-weight': 'bold')) do
+          text "Game Id"
+        end
+
+        span style: inline(table_style.merge('font-weight': 'bold')) do
+          text 'Name'
+        end
+
+        span style: inline(table_style) do
+          text ''
+        end
+      end
 
       new_games.each do |game|
         div do
-          span game.id
-          span game.user.name
-          a 'Join Game', href: app.path(game)
+          span style: inline(table_style) do
+            text game.id.to_s
+          end
+
+          span style: inline(table_style) do
+            text game.user.name
+          end
+
+          span style: inline(table_style) do
+            a 'Join Game', href: app.path(game)
+          end
         end
       end
+    end
+
+    def table_style
+      {
+        padding: '3px',
+        width: '100px',
+        display: 'inline-block',
+      }
     end
 
     def render_active_games
-      div 'Active Games'
+      hr
+      div style: inline(table_style.merge('font-weight': 'bold', 'width': '200px')) do
+        text 'Active Games'
+      end
+
+      div do
+        span style: inline(table_style.merge('font-weight': 'bold')) do
+          text "Game Id"
+        end
+
+        span style: inline(table_style.merge('font-weight': 'bold')) do
+          text 'Name'
+        end
+
+        span style: inline(table_style) do
+          text ''
+        end
+      end
 
       active_games.each do |game|
         div do
-          span game.id
-          span game.user.name
-          a 'Join Game', href: app.path(game)
+          span style: inline(table_style) do
+            text game.id.to_s
+          end
+
+          span style: inline(table_style) do
+            text game.user.name
+          end
+
+          span style: inline(table_style) do
+            a 'Join Game', href: app.path(game)
+          end
         end
       end
-    end
 
+    end
   end
 end
